63098180915c4b4d64693fa8dd2ab7d388e624ee
Javi <javiliyors@gmail.com>
2026-04-14 18:05:49 +0200

    Implementación del Factory de Movimiento.java #21

--- src/main/java/com/jlanzasg/novabank/model/Movimiento.java
+++ src/main/java/com/jlanzasg/novabank/model/Movimiento.java
@@ -1,52 +1,26 @@
 package com.jlanzasg.novabank.model;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-// Clase Movimiento
+import java.time.LocalDateTime;
+
+/**
+ * The type Movimiento.
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Movimiento {
 
-    // Variables
-    private Cuenta cuentaBancaria; // Variable CuentaBancaria para usar su (.getIban) como iban
-    private long id;
+    private Long id;
+    private Long cuentaId;
     private String tipo;
-    private double cantidad;
-    private LocalDateTime fecha;
-    private static long contadorMovimiento = 0L; // contador para el ID de movimientos
-    private static final DateTimeFormatter FORMATO_FECHA = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+    private Double cantidad;
 
-    // Constructor
-    public Movimiento(Cuenta cuentaBancaria, String tipo, double cantidad) {
-        this.id = ++contadorMovimiento; // Se incrementa el ID con el contador
-        this.cuentaBancaria = cuentaBancaria;
-        this.tipo = tipo;
-        this.cantidad = cantidad;
-        fecha = LocalDateTime.now(); // Se crea el movimiento con la fecha y hora actual
-    }
-
-    public long getId() { return id; }
-
-    public Cuenta getCuenta() { return cuentaBancaria; }
-
-    public String getTipo() {
-        return tipo;
-    }
-
-    public double getCantidad() {
-        return cantidad;
-    }
-
-    public LocalDateTime getFecha() { return fecha; }
-
-    @Override
-    public String toString() {
-
-        return "Movimiento{" +
-                "id=" + id +
-                ", iban='" + cuentaBancaria.getIban() + '\'' +
-                ", tipo='" + tipo + '\'' +
-                ", cantidad=" + cantidad +
-                ", fecha=" + fecha.format(FORMATO_FECHA) +
-                '}';
-    }
+    @Builder.Default
+    private LocalDateTime fecha = LocalDateTime.now();
 }
