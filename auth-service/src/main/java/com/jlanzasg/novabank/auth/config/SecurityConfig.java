f8357fc7fa736c8517a6cabaebad24fa8b4afcd0
Javier Lanzas González <jlangon187@iesfuengirola1.es>
2026-05-12 13:00:43 +0200

    feat(apigateway): fix auth

--- auth-service/src/main/java/com/jlanzasg/novabank/auth/config/SecurityConfig.java
+++ auth-service/src/main/java/com/jlanzasg/novabank/auth/config/SecurityConfig.java
@@ -9,7 +9,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
-@EnableWebFluxSecurity // <-- La clave: Seguridad Reactiva
+@EnableWebFluxSecurity
 public class SecurityConfig {
 
     @Bean
---
+++ exchange-rate-mock-service/src/main/java/com/jlanzasg/novabank/exchange/config/OpenApiConfig.java
@@ -0,0 +1,24 @@
+package com.jlanzasg.novabank.exchange.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(title = "API de NovaBank", version = "1.0"),
+        security = {@SecurityRequirement(name = "bearerAuth")},
+        servers = {@Server(url = "/", description = "API Gateway")}
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
+public class OpenApiConfig {
+}
